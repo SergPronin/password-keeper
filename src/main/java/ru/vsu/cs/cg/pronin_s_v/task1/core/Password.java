01b980006fdda6e2ad83b29a3bfa618d5365bfae
Sergey Pronin <sergeypronin.dev@gmail.com>
2025-09-17 18:01:43 +0300

    Добавлены поля, геттеры и сеттеры и toString в класс Password

--- src/main/java/ru/vsu/cs/cg/pronin_s_v/task1/core/Password.java
+++ src/main/java/ru/vsu/cs/cg/pronin_s_v/task1/core/Password.java
@@ -1,4 +1,46 @@
 package ru.vsu.cs.cg.pronin_s_v.task1.core;
 
 public class Password {
+    private String name;
+    private String username;
+    private String password;
+
+    public Password(String name, String username, String password) {
+        this.name = name;
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "Password{" +
+                "name='" + name + '\'' +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
 }
