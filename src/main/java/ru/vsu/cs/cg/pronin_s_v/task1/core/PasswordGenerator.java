f27e724f3aeaebaf38a695c311e027e7e8997e35
Sergey Pronin <sergeypronin.dev@gmail.com>
2025-09-17 18:19:52 +0300

    Добавлены основные поля в PasswordGenerator

--- src/main/java/ru/vsu/cs/cg/pronin_s_v/task1/core/PasswordGenerator.java
+++ src/main/java/ru/vsu/cs/cg/pronin_s_v/task1/core/PasswordGenerator.java
@@ -1,4 +1,11 @@
 package ru.vsu.cs.cg.pronin_s_v.task1.core;
 
+import java.security.SecureRandom;
+
 public class PasswordGenerator {
+    private static final String UPPERCASE = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    private static final String LOWERCASE = "abcdefghijklmnopqrstuvwxyz";
+    private static final String DIGITS = "0123456789";
+    private static final String SPECIAL = "!@#$%^&*()-_=+[]{}|;:,.<>?";
+
 }
